ba275a67a8e3078c8fb102cbc78d8b1ff1a1c99f
Anastasia <86796106+Anapkost@users.noreply.github.com>
2021-07-08 23:59:01 +0300

    Add files via upload

--- angryball.cpp
+++ angryball.cpp
@@ -14,100 +14,169 @@
 
 
 #include <iostream>
-#include <cmath>
-#include <clocale>
-#include <tuple>
-#include <list>
+#include <fstream> 
+#include <Windows.h> 
+#include <limits>
 
 using namespace std;
+const double g = 9, 81;
 
-const float g = 9.81;
-const float dt = 0.01;
+struct flight
+{
+    int mh = 120;
+    int mt = 57;
+    int* points;
+    double v = 0;
+    double a = 0;
+    double h = 0;
+    double t = 0;
+    double InputFlight(flight n_flight, double v0, double h0, double t0);
+    void createImage(int t, int h);
+    void initImage();
+    void saveToPnmFile(string fileName);
+    flight* next;
+};
 
-float rt(float t, float v0) {
-    return v0 * t - g * t * t / 2;
+class List
+{
+    flight* head;
+public:
+    List() :head(NULL) {};
+    ~List();
+    void AddFlight(flight n_flight);
+    void ShowFlight(double t0, double t1);
+};
+
+List::~List()
+{
+    while (head != NULL)
+    {
+        flight* temp;
+        temp = head->next;
+        delete head;
+        head = temp;
+    }
+}
+double flight::InputFlight(flight n_flight, double v0, double h0, double t0)
+{
+    t = t0;
+    h = h0 + v0 * t0 - g * t0 * t0 / 2;
+    v = v0 - g * t0;
+    a = -g;
+    return h;
+}
+void List::AddFlight(flight n_flight)
+{
+    flight* temp;
+    temp = new flight;
+    temp->next = head;
+    temp->t = n_flight.t;
+    temp->h = n_flight.h;
+    temp->v = n_flight.v;
+    temp->a = n_flight.a;
+    head = temp;
+}
+void TestTime(double t1, double t2)
+{
+    if ((t1 > t2) || (t1 < 0) || (t2 < 0))
+    {
+        cout << "error time" << endl;
+        return -1;
+    }
+    return 0;
+}
+void TestHight(double h0)
+{
+    double hMax = 20000;
+    if ((h0 < 0) || (h0 > hMax))
+    {
+        cout << "error heigh" << endl;
+        return -1;
+    }
+    return 0;
+}
+void List::ShowFlight(double t1, double t2)
+{
+    flight* temp;
+    temp = head;
+    while (temp != NULL)
+    {
+        if ((temp->t >= t1) && (temp->t <= t2))
+        {
+            cout << "t= " << temp->t << "\t\t";
+            cout << "h= " << temp->h << "\t\t";
+            cout << "v= " << temp->v << "\t\t";
+            cout << "a= " << temp->a << endl;
+        }
+        temp = temp->next;
+    }
+    cout << endl;
 }
 
-float xt(float t, float vx) {
-    return vx * t - g * t * t / 2;
+void flight::initImage()
+{
+    int n = mt * mh;
+    points = new int[n];
+    for (int i = 0; i < n; i++)
+        points[i] = 0;
 }
 
-float yt(float t, float vy) {
-    return vy * t - g * t * t / 2;
-}
-float at(float t, float vy, float vx) {
-    float xt1 = xt(t, vx);
-    float yt1 = yt(t, vy);
-    float ax = 2 * (xt1 - vx * t) / (t * t);
-    float ay = 2 * (yt1 - vy * t) / (t * t);
-    return sqrt(ax * ax + ay * ay);
+void flight::createImage(int t, int h)
+{
+    int n;
+    n = h * mt + t;
+    points[n] = 1;
+
 }
 
-float vt(float t, float v0, float sina) {
-    return sqrt(v0 * v0 - 2 * v0 * sina * g * t + g * g * t * t);
+void flight::saveToPnmFile(string fileName)
+{
+    fstream fout(fileName, ios::out);
+    int n;
+    fout << "P1" << endl;
+    fout << mt << " " << mh << endl;
+    for (int i = mh - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < mt; j++)
+        {
+            n = (i * mt + j);
+            fout << points[n] << " ";
+        }
+        fout << endl;
+    }
+    fout << endl;
+    fout.close();
 }
+
 int main()
 {
-    float vx, vy, v0;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    flight n_flight;
+    List lst;
+    double v0 = 10;
+    double g = 9, 81;
+    double m = 1;
+    double h0 = 100;
+    double t0;
+    double t1 = 0.5;
+    double h1 = 100;
+    double t1 = 0;
+    double t2 = 3;
+    double dt = 0.1;
+    t0 = t1;
+    
+    void TestHight(double h0);
 
-    cin >> vx >> vy;
-    v0 = sqrt(vx * vx + vy * vy);
-    float alpha;
-    alpha = acos(vx / v0);
-    float cosa = (vx / v0);
-    float sina = (vy / v0);
-
-    float m;
-    cin >> m;
-
-    float percentE;
-    cin >> percentE;
-
-    float tf;
-    cin >> tf;
-
-    float rmax = 2 * v0 * v0 * sina * cosa / g;
-    float Ek = (m * v0 * v0 / 2;
-
-    float t = dt;
-    tuple <float, float> tupleav;
-    list<tuple <float, float>> listav;
-    tupleav = make_tuple(v0, 0);
-    listav.push_back(tupleav);
-
-    float ht = -1, atf = -1, vtf = -1; //ошибка
-
-    while (xt(t, vx) >= rmax and xt(t, vx) <= rmax) {
-        float xt1 = xt(t, vx);
-        float yt1 = yt(t, vy);
-        float at1 = at(t, vy, vx);
-        float vt1 = vt(t, v0, sina);
-        tupleav = make_tuple(vt1, at1);
-        listav.push_back(tupleav);
-        if (t == tf or (t >= tf and t <= tf)) { ht = yt1; atf = at1; vtf = vt1; }
-        cout << "t= " << t << " xt= " << xt1 << " yt1= " << yt1 << " at1= " << at1 << " vt1= " << vt1 << endl;
-        t += dt;
+    while (n_flight.InputFlight(n_flight, v0, h0, t0) >= 0)
+        n_flight.initImage();
+    while (h1 >= 0)
+    {
+        h1 = n_flight.InputFlight(n_flight, v0, h0, t0);
+        n_flight.createImage(t0 * 10, h1);
+        lst.AddFlight(n_flight);
+        t0 = t0 + dt;
     }
-
-
-    float newEk = (percentE / 100) * Ek;
-    while (newEk <= 0) {
-        float v0new = sqrt(2 * newEk / m);
-        float vynew = v0new * cosa;
-        float vxnew = v0new * sina;
-        rmax = 2 * v0new * v0new * sina * cosa / g;
-        while (xt(t, vxnew) >= rmax and xt(t, vxnew) <= rmax) {
-            float xt1 = xt(t, vxnew);
-            float yt1 = yt(t, vynew);
-            float at1 = at(t, vynew, vxnew);
-            float vt1 = vt(t, v0, sina);
-            tupleav = make_tuple(vt1, at1);
-            listav.push_back(tupleav);
-            if (t == tf or (t >= tf and t <= tf)) { ht = yt1; atf = at1; vtf = vt1; }
-            cout << "t= " << t << " xt= " << xt1 << " yt1= " << yt1 << " at1= " << at1 << " vt1= " << vt1 << endl;
-            t += dt;
-        }
-    }
-
-    return 0;
+    lst.ShowFlight(t1, t2);
+    n_flight.saveToPnmFile("imageB.pnm");
 }
